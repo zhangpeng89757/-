e6c39456690ecdd3750d98220132a42907de2f74
root <2535752953@qq.com>
2023-12-13 21:23:51 +0800

    修改

--- src/components/keyenterprises/KeyEnterprises.vue
+++ src/components/keyenterprises/KeyEnterprises.vue
@@ -2,10 +2,26 @@
 <template>
     <div class="keyEnterprises-box-title background-image-box">
         <knowledgeTitle title="知识产权重点企业展示" text="家"></knowledgeTitle>
-        <div class="-content-box">
-                <div class="KeyEnterprises-content-item">
-                    <el-image class="KeyEnterprises-background" :src="GetPublicSource('enterprises-background.png')"></el-image>
-                </div>
+        <el-image class="KeyEnterprises-background" :src="GetPublicSource('enterprises-background.png')"></el-image>
+        <div class="enterprises-one">
+            <el-image class="enterprises" :src="GetPublicSource('enterprisestittle.png')"></el-image>
+            <div class="word-one" style="width: 180px;">知识产权优势企业</div>
+        </div>
+        <div class="enterprises-two">
+            <el-image class="enterprises" :src="GetPublicSource('enterprisestittle.png')"></el-image>
+            <div class="word-two" style="width: 180px;">专精特新企业</div>
+        </div>
+        <div class="enterprises-three">
+            <el-image class="enterprises" :src="GetPublicSource('enterprisestittle.png')"></el-image>
+            <div class="word-three" style="width: 180px;">知识产权贯标企业</div>
+        </div>
+        <div class="enterprises-four">
+            <el-image class="enterprises" :src="GetPublicSource('enterprisestittle.png')"></el-image>
+            <div class="word-four" style="width: 180px;">高新技术企业</div>
+        </div>
+        <div class="enterprises-five">
+            <el-image class="enterprises" :src="GetPublicSource('enterprisestittle.png')"></el-image>
+            <div class="word-five" style="width: 180px;">规上企业</div>
         </div>
     </div>
 </template>
@@ -14,7 +30,76 @@ import knowledgeTitle from '@/components/knowledgeTitle.vue'
 import { GetPublicSource } from '../../SystemCom/GetImages'
 </script>
 <style scoped>
-.keyEnterprises-box-title{
+.keyEnterprises-box-title {
     width: 50%;
+    position: relative;
 }
+
+.enterprises {
+    width: 70px;
+    height: 40px;
+}
+.enterprises-one {
+    position: absolute;
+    top: 20%;
+}
+
+.word-one {
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    font-size: 12px;
+}
+
+.enterprises-two {
+    position: absolute;
+    top: 20%;
+    left: 60%;
+}
+
+.word-two {
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    font-size: 12px;
+}
+
+.enterprises-three {
+    position: absolute;
+    top: 40%;
+}
+
+.word-three {
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    font-size: 12px;
+}
+
+.enterprises-four {
+    position: absolute;
+    top: 40%;
+    left: 60%;
+}
+
+.word-four{
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    font-size: 12px;
+}
+
+.enterprises-five {
+    position: absolute;
+    top: 60%;
+    left: 35%;
+}
+
+.word-five{
+    position: absolute;
+    top: 10%;
+    left: 30%;
+    font-size: 12px;
+}
+
 </style>
