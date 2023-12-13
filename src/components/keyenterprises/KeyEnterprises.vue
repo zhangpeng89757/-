096001382df60f51c0ea12da10c074a0daae6c3a
root <2535752953@qq.com>
2023-12-13 17:23:29 +0800

    还差知识产权重点企业

---
+++ src/components/keyenterprises/KeyEnterprises.vue
@@ -0,0 +1,20 @@
+<!-- 知识产权重点企业展示 -->
+<template>
+    <div class="keyEnterprises-box-title background-image-box">
+        <knowledgeTitle title="知识产权重点企业展示" text="家"></knowledgeTitle>
+        <div class="-content-box">
+                <div class="KeyEnterprises-content-item">
+                    <el-image class="KeyEnterprises-background" :src="GetPublicSource('enterprises-background.png')"></el-image>
+                </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import knowledgeTitle from '@/components/knowledgeTitle.vue'
+import { GetPublicSource } from '../../SystemCom/GetImages'
+</script>
+<style scoped>
+.keyEnterprises-box-title{
+    width: 50%;
+}
+</style>
--- src/views/knowledgeScreen/index.vue
+++ src/views/knowledgeScreen/index.vue
@@ -1,47 +1,58 @@
 <template>
   <div class="knowledgeScreen_box">
-  <div class="box_left">
-    <knowledgeTitle title="知识产权创造结果"></knowledgeTitle>
-  </div>
-  <div class="box_min">{{ test }}</div>
-  <div class="box_right">{{ test }}</div>
+    <div class="box_left">
+      <knowledgeTitle title="知识产权创造结果"></knowledgeTitle>
+    </div>
+    <div class="box_min"></div>
+    <div class="box_right">
+      <div style="display: flex;">
+        <KeyEnterprises></KeyEnterprises>
+        <KnowledgeWarning></KnowledgeWarning>
+      </div>
+       
+      
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import knowledgeTitle from '@/components/knowledgeTitle.vue'
-
-let test = "知识大屏"
+// import knowledgeTitle from '@/components/knowledgeTitle.vue'
+import KnowledgeWarning from '../../components/KnowledgeWarning.vue';
+import KeyEnterprises from '../../components/KeyEnterprises/KeyEnterprises.vue';
+// let test = "知识大屏"
 </script>
 <style lang="less" scoped>
-  .knowledgeScreen_box{
-    width: 100%;
-   
-    .box_left{
-      width: 26vw;
+.knowledgeScreen_box {
+  width: 100%;
+
+  .box_left {
+    width: 26vw;
     height: 89vh;
     position: fixed;
     top: 6vh;
     left: 50px;
-      background: red;
-    }
-    .box_min{
-      padding-top: 60px;
+    background: red;
+  }
+
+  .box_min {
+    padding-top: 60px;
     box-sizing: border-box;
     width: 42vw;
     position: fixed;
     left: 50%;
     transform: translateX(-50%);
-      box-sizing: border-box;
-      height: 88%;
-      background: #666;
-    }
-    .box_right{
-      width: 26vw;
+    box-sizing: border-box;
+    height: 88%;
+    background: #666;
+  }
+
+  .box_right {
+    width: 26vw;
     height: 89vh;
     position: fixed;
     top: 6vh;
     right: 50px;
-      background: blue;
-    }
   }
+
+
+}
 </style>
